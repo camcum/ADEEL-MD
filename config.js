8ff5c6648ae13e1fedaa6115cf457c8b6401a918
camcum <160652910+camcum@users.noreply.github.com>
2025-11-25 19:24:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ALI-MD~DZFD0TzI#T12IjIBCRKng-GKY_LEYROhA7CIOWVYL1NRLZAz_LcQ",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
